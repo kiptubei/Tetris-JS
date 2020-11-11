3451d8e1c9e0c181a2b094eb6703ccd3f9f968f6
kiptubei <kiptubei@gmail.com>
2020-11-11 18:01:39 +0300

    Added moveleft and moveright actions

--- app.js
+++ app.js
@@ -72,6 +72,21 @@ document.addEventListener("DOMContentLoaded", () => {
   // Move tetromino down
   let timerId = setInterval(moveDown, 1000);
 
+  // assign functions to keycodes
+  function control(e){
+    if(e.keyCode === 37){
+      moveLeft();
+    } else if(e.keyCode === 38){
+      //rotate()
+    }else if(e.keyCode === 39){
+      moveRight()
+    }else if (e.keyCode === 48){
+      moveDown()
+    }
+  }
+
+  document.addEventListener('keyup',control);
+
   function moveDown() {
     undraw();
     currentPosition += width;
@@ -81,14 +96,59 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // freeze function to stop tetromino when it gets to the bottom
   function freeze() {
-    if (current.some((index) => squares[currentPosition + index + width].classList.contains("taken")))
-    {
-      current.forEach(item => squares[currentPosition + item].classList.add("taken"));
-        // start a new tetromino falling
-        random = Math.floor(Math.random() * theTetrominoes.length);
-        current = theTetrominoes[random][currentRotation];
-        currentPosition = 4;
-        draw();
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + width].classList.contains("taken")
+      )
+    ) {
+      current.forEach((item) =>
+        squares[currentPosition + item].classList.add("taken")
+      );
+      // start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
     }
   }
+
+  // Move tetrominoes left unless at the edge or blocked
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+    if (!isAtLeftEdge) currentPosition -= 1;
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
+    }
+
+    draw();
+  }
+
+  // Move tetrominoes left unless at the edge or blocked
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width -1
+    );
+    if (!isAtRightEdge) currentPosition += 1;
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition -= 1;
+    }
+
+    draw();
+  }
+
+
 });
